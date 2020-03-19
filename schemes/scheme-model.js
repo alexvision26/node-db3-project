a803cdfa75c1140e5c1e76e678baed50e58a98c5
Alex Miller <alexmillerprojects@gmail.com>
2020-03-18 17:06:42 -0700

    finished project. mvp

---
+++ schemes/scheme-model.js
@@ -0,0 +1,52 @@
+const db = require("../data/db.config");
+
+module.exports = {
+  find,
+  findById,
+  findSteps,
+  add,
+  update,
+  remove
+};
+
+function find() {
+  return db("schemes");
+}
+
+function findById(id) {
+  return db("schemes")
+    .where({ id })
+    .first();
+}
+
+function findSteps(id) {
+  return db("steps")
+    .where("steps.scheme_id", "=", id)
+    .orderBy("steps.step_number");
+}
+
+function add(scheme) {
+  return db("schemes")
+    .insert(scheme)
+    .then(ids => {
+      return findById(ids[0]);
+    });
+}
+
+function update(changes, id) {
+  return db
+    .select("schemes.scheme_name")
+    .from("schemes")
+    .where({ id })
+    .update(changes)
+    .then(() => {
+      return findById(id);
+    });
+}
+
+function remove(id) {
+  const toDel = findById(id);
+  return db("schemes")
+    .where("schemes.id", id)
+    .del();
+}
